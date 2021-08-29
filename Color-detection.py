64286679503537509168c39b63865cc9dae697f6
Shreya Somani <57498575+ShreyaSomani@users.noreply.github.com>
2021-08-29 14:31:41 +0530

    Update Color-detection.py

--- Color-detection.py
+++ Color-detection.py
@@ -109,44 +109,41 @@ def display(img):
     cv2.line(img, (0,int(frameHeight / 2) - deadZone), (frameWidth,int(frameHeight / 2) - deadZone), (255, 255, 0), 3)
     cv2.line(img, (0, int(frameHeight / 2) + deadZone), (frameWidth, int(frameHeight / 2) + deadZone), (255, 255, 0), 3)
  
-    while True:
-        
-        img = cap.read()
-        imgContour = img.copy()
-        imgHsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+while True:
  
-        h_min = cv2.getTrackbarPos("HUE Min","HSV")
-        h_max = cv2.getTrackbarPos("HUE Max", "HSV")
-        s_min = cv2.getTrackbarPos("SAT Min", "HSV")
-        s_max = cv2.getTrackbarPos("SAT Max", "HSV")
-        v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
-        v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
-        print(h_min)
+    _, img = cap.read()
+    imgContour = img.copy()
+    imgHsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
  
-        lower = np.array([h_min,s_min,v_min])
-        upper = np.array([h_max,s_max,v_max])
-        mask = cv2.inRange(imgHsv,lower,upper)
-        result = cv2.bitwise_and(img,img, mask = mask)
-        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    h_min = cv2.getTrackbarPos("HUE Min","HSV")
+    h_max = cv2.getTrackbarPos("HUE Max", "HSV")
+    s_min = cv2.getTrackbarPos("SAT Min", "HSV")
+    s_max = cv2.getTrackbarPos("SAT Max", "HSV")
+    v_min = cv2.getTrackbarPos("VALUE Min", "HSV")
+    v_max = cv2.getTrackbarPos("VALUE Max", "HSV")
+    print(h_min)
  
-        imgBlur = cv2.GaussianBlur(result, (7, 7), 1)
-        imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-        threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-        threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
-        imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
-        kernel = np.ones((5, 5))
-        imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
-        getContours(imgDil, imgContour)
-        display(imgContour)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHsv,lower,upper)
+    result = cv2.bitwise_and(img,img, mask = mask)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
  
-        stack = stackImages(0.7,([img,result],[imgDil,imgContour]))
+    imgBlur = cv2.GaussianBlur(result, (7, 7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    getContours(imgDil, imgContour)
+    display(imgContour)
  
-        cv2.imshow('Horizontal Stacking', stack)
-        if cv2.waitKey(1) &amp and 0xFF == ord('q'):
-            break
+    stack = stackImages(0.7,([img,result],[imgDil,imgContour]))
  
- 
-       
+    cv2.imshow('Horizontal Stacking', stack)
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        break
  
 cap.release()
 cv2.destroyAllWindows()
